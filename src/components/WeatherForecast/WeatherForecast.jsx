c4b5360d7b8a9508c62d8b7950a7411ffea71fbf
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-03-11 17:35:50 -0500

    setup

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,25 @@
+// src/components/WeatherForecast/WeatherForecast.jsx
+
+import { useState } from 'react'
+import './WeatherForecasts.css'
+
+const WeatherForecast = () => {
+
+    return {
+        // beeeemis
+
+
+
+        // <div className="weather">
+        //   <h2>Day of the Week</h2>
+        //   <img src="" alt="" />
+        //   <p><span>conditions: </span>current weather conditions</p>
+        //   <p><span>time: </span>time of day</p>
+        // </div>
+
+
+
+    };
+};
+
+export default WeatherForecast;
